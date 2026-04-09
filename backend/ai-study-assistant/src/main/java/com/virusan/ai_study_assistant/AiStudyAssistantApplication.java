c7b4b490cb0fdccb5217a70c2e143aebedf8efc7
virusan-T <thavamviru@gmail.com>
2026-04-09 19:26:55 +0530

    Initial project setup

---
+++ backend/ai-study-assistant/src/main/java/com/virusan/ai_study_assistant/AiStudyAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.virusan.ai_study_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStudyAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiStudyAssistantApplication.class, args);
+	}
+
+}
---
+++ backend/ai-study-assistant/src/test/java/com/virusan/ai_study_assistant/AiStudyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virusan.ai_study_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudyAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
